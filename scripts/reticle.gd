7afefc4956527aca5116e416c720ef78af231200
Spencer Fisher <spencer.fisher@billgo.com>
2024-11-16 14:51:50 -0800

    lerp reticule

--- scripts/reticle.gd
+++ scripts/reticle.gd
@@ -7,6 +7,8 @@ extends CenterContainer
 @export var DOT_RADIUS = 1.0
 @export var DOT_COLOR : Color = Color.WHITE
 
+var speed : float = 0
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	queue_redraw()
@@ -23,7 +25,8 @@ func adjust_reticle_lines():
 	var origin = Vector3(0,0,0)
 	var pos = Vector2(0,0)
 	#Distance traveled in one frame
-	var speed = origin.distance_to(vel)
+	
+	speed = lerp(speed, origin.distance_to(vel), 0.075)
 
 #Adjust Reticle Line Posiition
 	RETICLE_LINES[0].position = lerp(RETICLE_LINES[0].position, pos + Vector2(0, -speed * RETICLE_DISTANCE), RETICLE_SPEED) #Top
